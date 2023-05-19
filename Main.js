bad622493fce9d90457a5bf641b113fb1a73b2d2
timonim <78536544+timonim@users.noreply.github.com>
2023-05-19 14:22:39 +0300

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,30 @@
+let heroPill = document.getElementById("hero-pill");
+let words = ["Products", "UI/UX", "Web", "Apps", "3D", "Marketing", "Branding", "Packagings", "Social", "AI Art"];
+let index = 0;
+let prevWord = words[0]; // add a variable to store the previous word
+
+setInterval(function() {
+  let newWord = words[index];
+  if (newWord !== prevWord) { // check if the word has changed
+    heroPill.innerHTML = newWord;
+    heroPill.classList.add("changed"); // add a class to trigger the animation
+    setTimeout(function() {
+      heroPill.classList.remove("changed"); // remove the class after the animation
+    }, 300);
+    prevWord = newWord; // store the new word as the previous word
+  }
+  index = (index + 1) % words.length;
+}, 1000);
+
+// hidden text
+
+let button = document.getElementById("my-button");
+let paragraph = document.getElementById("my-paragraph");
+
+button.addEventListener("click", function() {
+  if (paragraph.classList.contains("hidden")) {
+    paragraph.classList.remove("hidden");
+  } else {
+    paragraph.classList.add("hidden");
+  }
+});

7eb3c1e4776c3d1ea190a568c1fefb004d181db9
Timp <Timp@timp.com>
2023-07-15 19:33:49 +0300

    Trollz page and ui improve

--- mainscript.js
+++ mainscript.js
@@ -1,14 +1,30 @@
+// window.addEventListener('load', function() {
+//   document.body.classList.add('loaded');
+// });
+
 let heroPill = document.getElementById("hero-pill");
-let words = ["Products", "UI/UX", "Web", "Apps", "3D", "Marketing", "Branding", "Packagings", "Social", "AI Art"];
+let words = [
+  "Products",
+  "UI/UX",
+  "Web",
+  "Apps",
+  "3D",
+  "Marketing",
+  "Branding",
+  "Packagings",
+  "Social",
+  "AI Art",
+];
 let index = 0;
 let prevWord = words[0]; // add a variable to store the previous word
 
-setInterval(function() {
+setInterval(function () {
   let newWord = words[index];
-  if (newWord !== prevWord) { // check if the word has changed
+  if (newWord !== prevWord) {
+    // check if the word has changed
     heroPill.innerHTML = newWord;
     heroPill.classList.add("changed"); // add a class to trigger the animation
-    setTimeout(function() {
+    setTimeout(function () {
       heroPill.classList.remove("changed"); // remove the class after the animation
     }, 300);
     prevWord = newWord; // store the new word as the previous word
@@ -21,6 +37,6 @@ setInterval(function() {
 let button = document.getElementById("my-button");
 let paragraph = document.getElementById("my-paragraph");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   paragraph.classList.toggle("hidden");
 });
